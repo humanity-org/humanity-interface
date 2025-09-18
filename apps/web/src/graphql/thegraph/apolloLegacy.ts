d46d28d7f9b4da24b032a0316c2626d68771612f
den <dzianis.smalonski@protofire.io>
2025-09-18 10:55:07 +0200

    Humanity

--- apps/web/src/graphql/thegraph/apolloLegacy.ts
+++ apps/web/src/graphql/thegraph/apolloLegacy.ts
@@ -18,7 +18,7 @@ const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [UniverseChainId.AbstractMainnet]: 'https://graph.swap.w3us.site/subgraphs/name/abstract/uniswap-v3',
   [UniverseChainId.AnimeTestnet]: 'https://graph-node.replace.domain/subgraphs/name/anime-testnet/v3-subgraph',
   [UniverseChainId.Anime]: 'https://graph-node.replace.domain/subgraphs/name/anime/v3-subgraph',
-  [UniverseChainId.Humanity]: 'https://graph.staging.swap.humanity.org/subgraphs/name/humanity/v3-subgraph',
+  [UniverseChainId.Humanity]: 'https://graph.staging.swap.humanity.org/subgraphs/name/humanity-mainnet/v3-subgraph',
   [UniverseChainId.Mode]: 'https://graph-node.replace.domain/subgraphs/name/mode/v3-subgraph',
 }
 
